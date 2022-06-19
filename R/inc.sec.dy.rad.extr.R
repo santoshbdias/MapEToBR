21f778b80effc96a8d49ccf87d1d4ac42021ba14
santoshbd <santosdiasagro@gmail.com>
2022-06-19 18:29:56 -0300

    6 commit

--- R/inc.sec.dy.rad.extr.R
+++ R/inc.sec.dy.rad.extr.R
@@ -7,23 +7,13 @@
 #'@param datfram um Data Frame gerado com a função ETo_BR do pacote com os as coordenadas x e y escritas Lat e Long especificamente, além da data!
 #'
 #'@example
-#'rad.extr(datfram,stackday,shapflbr)
+#'inc.sec.dy.rad.extr(datfram,stackday,shapflbr)
 #'
 #'@export
 #'@return Returns a stack raster day modeling
 #'@author Santos Henrique Brant Dias
 #'
 #'
-
-
-if(!require("pacman")) install.packages("pacman");pacman::p_load(
-  raster, rgdal, terra, MapEToBR)
-
-stackday <- raster::stack('D:/OneDrive/Doutorado/Tese/Base_Dados_BR/bspredDay.grd')
-
-datfram <- ETo_BR(date=Sys.Date()-1)
-
-shapflbr <- readOGR(dsn = 'D:/OneDrive/Doutorado/Tese/Base_Dados_BR/Shape_Brasil/Shape_Brasil.shp')
 
 
 inc.sec.dy.rad.extr <- function(datfram,stackday,shapflbr) {
@@ -39,10 +29,8 @@ inc.sec.dy.rad.extr <- function(datfram,stackday,shapflbr) {
   lamb=23.45*sin((360/365*(dj-80))*pi/180)#Declinação solar
   dD2=1+0.033*cos((360/365)*dj*pi/180)#Constante solar - dist?ncia terra sol
 
-
-  lat <- init(stackday$meday0101, 'y')
+  lat <- init(stackday[[1]], 'y')
   names(lat)<-'Latitude'
-
 
   Hn=acos((-tan(lamb*pi/180)*tan(lat*pi/180)))*180/pi#Angulo zenital
   Qo=37.6*dD2*(pi/180*Hn*sin(lamb*pi/180)*sin(lat*pi/180)+cos(lamb*pi/180)*cos(lat*pi/180)*sin(Hn*pi/180))
